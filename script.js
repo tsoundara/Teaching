ddba50c2be3c1434169a602f4d1cf9dabedcc0c5
tsoundara <tommysoundara9@gmail.com>
2026-02-12 23:30:00 -0600

    Reformatting each step structure,  new styling for drop down effect, js listener to close other tabs when opening a new one

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-addEventListener
+const headers = document.querySelectorAll('.stepHeader');  //select all headers 
+
+headers.forEach(header => {
+    header.addEventListener('click', () => {
+        const parent = header.parentElement;                //grab the parent of the header element (stepDropdwon)
+        const isOpen = parent.classList.contains('active');         //check if the parent has the active class (is it open?)
+
+        document.querySelectorAll('.stepDropdown').forEach(dropdown => {    //select all dropdowns and loop through them
+            dropdown.classList.remove('active');   //remove the active class from all dropdowns (all dropdowns will be closed)
+        });
+        
+        if(!isOpen){            //if the dropdown was not open, add the active class to the parent (open the clicked dropdown)
+            parent.classList.add('active');    //add the active class to the parent (open the clicked dropdown)
+        }
+    });
+});
